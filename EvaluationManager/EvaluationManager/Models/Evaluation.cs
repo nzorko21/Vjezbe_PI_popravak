fcd96439d05284e3efd2f872ec21a4d5dd84a0c7
Programsko Inzenjerstvo <pi_foi@outlook.com>
2023-05-15 18:39:16 +0200

    Dodana klasa za evaluation

---
+++ EvaluationManager/EvaluationManager/Models/Evaluation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evaluation_Manager.Models {
+    public class Evaluation {
+
+        prop public Activity Activity { get; set; }
+
+        public Student Student { get; set; }
+
+        public Teacher Evalutaor { get; set; }   
+
+        public DateTime EvaluationaDate { get; set; }
+
+        public int Points { get; set; }
+
+        public bool IsSufficientForGrade() {
+
+            return Points >=Activity.MinPointsForGrade;
+        }
+
+        public bool IsSufficientForSignature() {
+
+            return Points >= Activity.MinPointsForSignature;
+        }
+
+    }
+}
