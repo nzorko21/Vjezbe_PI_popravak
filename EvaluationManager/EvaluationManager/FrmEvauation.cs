a043c4529f536e02e8c2be95ac70bf7bc5ade0a6
Nikola <Nikola@LAPTOP-LTJIPVJS>
2023-05-15 04:55:07 +0200

    promjena

--- EvaluationManager/EvaluationManager/FrmEvauation.cs
+++ EvaluationManager/EvaluationManager/FrmEvauation.cs
@@ -6,7 +6,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Runtime.CompilerServices;
+using System.Runtime.CompilerServices; 
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
